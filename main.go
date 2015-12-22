ea752562ed4733d4ce2bc31ef2b189f34f5ed40e
Jon Burman <jonathanburman@timeout.com>
2015-12-22 17:26:25 +0000

    First Commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main 
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+
+var apiKey string = "APIKEY"
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+		TempMax float64 `json:"temp_max"`
+		} `json:"main"`
+	}
+
+
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData 
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
+}
+
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/weather/", weather)
+	http.ListenAndServe(":8000", nil)
+}
+
+
+func weather(w http.ResponseWriter, r *http.Request) {
+	city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+	data, err := query(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
+}
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain( t *testing.T ) {
+	if false {
+		t.Errorf("Test failed as true doesn't equal false")
+		}
+}
